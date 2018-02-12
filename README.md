54336b8d8dbb60cba44abaab55ee863a7d0111eb
Shruthi Patlolla <S531376@nwmissouri.edu>
2018-02-11 21:01:38 -0600

    option to choose the level

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # shruthipatlolla.github.io
+
+BullsNCows
+A simple game to guess the random generated number by the system and it will provide hints based on your input number.
+Can be played by one player. 
+
+Getting Started
+Clone the entire project and run the home.html file to access the home page of application.
+
+Prerequisites
+A web browser (chrome,firefox etc..)is sufficient to run this application in your local.
+
+
+To play the game
+Example
+system generated number is 92
+If Your first is guess is 27
+    it will give the hints as you got 0(there are no numbers you have guessed are at right position) bulls and 1(you have guessed the one number but it is not in correct position) cow which means .
+
+Running the tests
+run the test/test.html file to check the tests.
+
+
+Deployment
+The application is deployed in githubpages . 
+please find at https://shruthipatlolla.github.io./home.html
+
+
+
+Authors
+Shruthi Patlolla - Initial work.
+
--- bnc.js
+++ bnc.js
@@ -1,10 +1,39 @@
+var levelSelected ;
+var randomValue ;
+var noOfDigits;
 
-function calc() {
-    var name = document.getElementById("value1").value;
-    document.getElementById("pTag").innerHTML = "hello, Welcome "+name;
+function play(){
+    $("#play").removeAttr("style").hide();
+    window.alert("Select the level");
+    $("#level").show();
+
+}
+
+
+function selectLevel(selectValue) {
+    levelSelected = selectValue.id;
+    $("#level").removeAttr("style").hide();
+    randomValue = randomValue();
+    $("#digits").html("Enter a "+noOfDigits+" digit value");
+    $("#formIn").show();
     
+}
+  
+  
+  function randomValue(){
+      if(levelSelected=="easy" ){
+        noOfDigits = 2;
+        return  Math.floor(Math.random()*90) + 10;
+      }
+      else if(levelSelected == "medium"){
+        noOfDigits = 3;
+          return  Math.floor(Math.random()*900) + 100;
+      }
+      else{
+        noOfDigits = 4;
+        return Math.floor(Math.random()*9000) + 1000;
+      }
   }
-  var randomValue =  Math.floor(Math.random()*9000) + 1000;
   
   var numberOfTrails = 0;
 
@@ -20,10 +49,10 @@ function calc() {
    // if(form.isValid()){
     numberOfTrails++;
     //Checking for bulls
-    for(var i=0;i<4;i++){
+    for(var i=0;i<noOfDigits;i++){
         if(tempRandomNumber % 10 == tempEnteredNumber % 10) {
             bulls++;
-            if(bulls == 4) {
+            if(bulls == noOfDigits) {
                 alert("Congrats, u r done dis in "+numberOfTrails+" trails");
                 window.location.reload();
             }
@@ -36,13 +65,13 @@ function calc() {
     var tempEnteredNumber = number ;
     var tempRandomNumber = randomNo;
     var randDigits = [], enteredDigits = [];
-    for(var i = 0; i < 4; i++){
+    for(var i = 0; i < noOfDigits; i++){
         enteredDigits.push(tempEnteredNumber % 10);
         randDigits.push(tempRandomNumber % 10);
         tempEnteredNumber = parseInt(tempEnteredNumber / 10);
         tempRandomNumber = parseInt(tempRandomNumber / 10);
     }
-    for(var i = 0; i < 4; i++) {
+    for(var i = 0; i < noOfDigits; i++) {
         var digit = enteredDigits[i];
         var position = randDigits.indexOf(digit);
         if(position >= 0) {
@@ -55,7 +84,7 @@ function calc() {
     values[0] = [number, bulls, cows];
     var myTable = document.getElementById("myTable");
    
-    var row = myTable.insertRow(0);
+    var row = myTable.insertRow(-1);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
